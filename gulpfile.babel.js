695dd4a32fa8f4e5026c9cf6f28070a821b6e463
Jeremy Fleischman <jeremyfleischman@gmail.com>
2025-01-01 22:28:41 -0800

    Fix `make lint` and `make format`, enforce `biome check` in Nix/CI
    The `make` commands were broken because we didn't have `biome` listed as
    a dependency, and we didn't have a `biome.json` file.
    
    There's unfortunately still some inconsistency here in that `nix fmt`
    and `nix flake check` are going to use whatever version of biome is
    present in `nixpkgs`, whereas `make lint` is going to use the version in
    our `package-lock.json`. We could make this consistent, but I'm
    inclined to wait for it to be a problem.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,28 +1,28 @@
-var del = require("del");
-var gulp = require("gulp");
-var path = require("path");
-var jimp = require("gulp-jimp");
-var through = require("through2");
-var rename = require("gulp-rename");
-var svg2png = require("gulp-svg2png");
-var iconfont = require("gulp-iconfont");
-var bufferstreams = require("bufferstreams");
-var child_process = require("child_process");
-var consolidate = require("gulp-consolidate");
+const del = require("del");
+const gulp = require("gulp");
+const path = require("node:path");
+const jimp = require("gulp-jimp");
+const through = require("through2");
+const rename = require("gulp-rename");
+const svg2png = require("gulp-svg2png");
+const iconfont = require("gulp-iconfont");
+const bufferstreams = require("bufferstreams");
+const child_process = require("node:child_process");
+const consolidate = require("gulp-consolidate");
 
-var FONT_NAME = "cubing-icons";
-var SVG_FILES = "svgs/*/*.svg";
-var STATIC_FILES = "static/*/*";
-var TEMPLATE_FILES = "templates/*.lodash";
-var SRC_FILES = [SVG_FILES, TEMPLATE_FILES];
+const FONT_NAME = "cubing-icons";
+const SVG_FILES = "svgs/*/*.svg";
+const STATIC_FILES = "static/*/*";
+const TEMPLATE_FILES = "templates/*.lodash";
+const SRC_FILES = [SVG_FILES, TEMPLATE_FILES];
 
-var runTimestamp = Math.round(Date.now() / 1000);
+const runTimestamp = Math.round(Date.now() / 1000);
 
 const defaultTask = gulp.parallel(copySvgs, copyStaticFiles, css);
 export default defaultTask;
 
 export function css() {
-  var fontCss = fontCssPipe();
+  const fontCss = fontCssPipe();
   return (
     gulp
       .src(SVG_FILES)
@@ -35,7 +35,7 @@ export function css() {
       // svg2png looks for files on the filesystem by name, so we cannot
       // rename until after svg2png. We should really fix this bug in svg2png.
       .pipe(
-        through.obj(function (file, enc, next) {
+        through.obj((file, enc, next) => {
           file.path = path.join(
             path.dirname(file.path),
             file.relative.replace("/", "-"),
@@ -63,7 +63,7 @@ export function css() {
           fontHeight: 1000,
         }),
       )
-      .on("glyphs", function (glyphs, options) {
+      .on("glyphs", (glyphs, options) => {
         // Stash glyphs so we can generate the CSS at the end.
         fontCss.glyphs = glyphs;
 
@@ -99,12 +99,12 @@ export function clean() {
 }
 
 function fontCssPipe() {
-  var fontFiles = [];
+  const fontFiles = [];
   return through.obj(
-    function (file, enc, cb) {
+    (file, enc, cb) => {
       file.contents
         .pipe(
-          new bufferstreams(function (err, buf, cb) {
+          new bufferstreams((err, buf, cb) => {
             if (err) {
               throw err;
             }
@@ -117,11 +117,9 @@ function fontCssPipe() {
     },
     function (cb) {
       // Modified from https://www.npmjs.com/package/gulp-iconfont
-      var fontUrls = {
-        woff:
-          "data:application/x-font-woff;charset=utf-8;base64," + fontFiles.woff,
-        ttf:
-          "data:application/x-font-ttf;charset=utf-8;base64," + fontFiles.ttf,
+      const fontUrls = {
+        woff: `data:application/x-font-woff;charset=utf-8;base64,${fontFiles.woff}`,
+        ttf: `data:application/x-font-ttf;charset=utf-8;base64,${fontFiles.ttf}`,
       };
       gulp
         .src("templates/cubing-icons.css.lodash")
@@ -141,8 +139,8 @@ function fontCssPipe() {
 }
 
 function bmp2svg() {
-  return through.obj(function (file, enc, next) {
-    var potraceProcess = child_process.spawn(
+  return through.obj((file, enc, next) => {
+    const potraceProcess = child_process.spawn(
       "potrace",
       ["-o", "-", "-b", "svg"],
       {
@@ -154,14 +152,14 @@ function bmp2svg() {
     potraceProcess.stdin.end();
 
     // TODO - there must be some way of avoiding this...
-    var buffer = new Buffer.alloc(0);
-    potraceProcess.stdout.on("data", function (data) {
+    let buffer = new Buffer.alloc(0);
+    potraceProcess.stdout.on("data", (data) => {
       buffer = Buffer.concat([buffer, data]);
     });
 
-    potraceProcess.once("close", function (code) {
+    potraceProcess.once("close", (code) => {
       if (code !== 0) {
-        next(new Error("potrace exited with code " + code, null));
+        next(new Error(`potrace exited with code ${code}`, null));
       } else {
         file.extname = ".svg";
         file.contents = buffer;
--- nix/formatting.nix
+++ nix/formatting.nix
@@ -2,14 +2,29 @@
 {
   imports = [ inputs.treefmt-nix.flakeModule ];
 
-  perSystem.treefmt = {
-    flakeCheck = true;
-    programs = {
-      nixfmt.enable = true;
-      biome = {
-        enable = true;
-        settings.formatter.indentStyle = "space";
+  perSystem =
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+    {
+      treefmt = {
+        flakeCheck = true;
+        programs = {
+          nixfmt.enable = true;
+          biome.enable = true;
+        };
+        settings.formatter = {
+          "biome-check" = {
+            command = lib.getExe pkgs.biome;
+            options = [
+              "check"
+            ];
+            inherit (config.treefmt.programs.biome) includes;
+          };
+        };
       };
     };
-  };
 }
