58272e011f98dc22d7d053b413f5fb4f1cc39c80
Lucas Garron <code@garron.net>
2025-08-01 00:24:44 -0700

    Add a test that all source SVGs explicitly set `width` and `height` attributes to `"500"`. (#186)

---
+++ script/test-svg-dimensions.spec.ts
@@ -0,0 +1,24 @@
+import { expect, test } from "bun:test";
+import { Glob, file } from "bun";
+import { JSDOM } from "jsdom";
+
+test("SVG files are all 500×500", async () => {
+  let numSVGs = 0;
+  for await (const svgFile of new Glob("./src/svg/*/*.svg").scan()) {
+    numSVGs++;
+    const svgElem = new JSDOM(
+      await file(svgFile).text(),
+    ).window.document.querySelector("svg");
+    expect(svgElem).not.toBeNull();
+    expect(svgElem?.getAttribute("width")).toEqual("500");
+    expect(svgElem?.getAttribute("height")).toEqual("500");
+  }
+
+  /**
+   * - We could hardcode the number in the test here, but that would require every new SVG contribution to increment the expected value.
+   * - We could compare against the built set of icons, but that introduces test dependencies.
+   *
+   * So instead we just hardcode a reasonable lower bound check.
+   */
+  expect(numSVGs).toBeGreaterThan(50);
+});
